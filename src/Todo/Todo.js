6df1026ee0df4b19c78627465ea68fcdb82117c2
marcin witkowski <marcin.witkowski@e-qsg.com>
2020-12-28 19:37:02 +0100

    react class end

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -9,16 +9,49 @@ const redStyle = {
 };
 
 
-function Todo(props) {
-    return (
-        props.done === true ?
-            <li style={greenStyle} >
-                {props.value}
-            </li> :
-            <li style={redStyle} >
-                {props.value}
-            </li>
-    );
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.setDone = this.setDone.bind(this);
+        this.state = {done: null};
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        if (state.done === null)
+            return {done: props.done}; else return {};
+    }
+
+    setDone = () => {
+        if (this.state.done) {
+            this.setState({done: false}, this.props.update(this.props.id, this.state.done));
+        } else {
+            this.setState({done: true}, this.props.update(this.props.id, this.state.done));
+        }
+
+    }
+
+    render() {
+
+        const {value} = this.props;
+        const {done} = this.state;
+
+        return (
+            <div>
+                {done === true ?
+                    <li style={greenStyle}>
+                        {value}
+                    </li> :
+                    <li style={redStyle}>
+                        {value}
+                    </li>}
+                <button onClick={this.setDone}>
+                    Change status
+                </button>
+            </div>
+        )
+
+    }
+    ;
 }
 
 export default Todo;
--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import styled from 'styled-components';
 import Todo from "../Todo/Todo";
 
-const greenStyle = {
-    color: 'green',
-};
-
-const Undone = styled.li`
-  color: red;
-`;
-
 class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        let t1 = {id: 0, value: "Learn React", done: true};
+        let t2 = {id: 1, value: "Pass Programming Laboratory", done: false};
+        let t3 = {id: 2, value: "Have a nice holiday", done: false};
+        this.state = {todosList: [t1, t2, t3]};
+        this.addTodo = this.addTodo.bind(this);
+        this.counter = 2;
+    };
+
+    updateChild = (id, done) => {
+        let arrAfterUpdate = this.state.todosList.map(function (item) {
+            if(item.id === id) item.done = done;
+            return item;
+        });
+        this.setState({todosList: arrAfterUpdate});
+        console.log(arrAfterUpdate);
+    };
+
+
+    addTodo = () => {
+        this.counter++;
+        let newelement = {id: this.counter, value: this.state.newTodo, done: false};
+        this.setState(prevState => ({
+            todosList: [...prevState.todosList, newelement]
+        }));
+    };
+
+    handleRemove = id => {
+        let arrAfterDel = this.state.todosList.filter(function (item) {
+            return item.id !== id
+        });
+        this.setState({todosList: arrAfterDel});
+    }
+
+    myChangeHandler = (event) => {
+        this.setState({newTodo: event.target.value});
+    }
+
     render() {
+
+        const {todosList} = this.state;
+
+        let todos = todosList.map(todo => {
+            return (<ul key={todo.id}>
+                <Todo value={todo.value}
+                      done={todo.done}
+                      id = {todo.id}
+                      update={() => this.updateChild}  />
+                <button type="button" onClick={() => this.handleRemove(todo.id)}>
+                    Remove
+                </button>
+            </ul>);
+        })
+
         return (
             <div className="TodoList">
                 <h1>Todo List {this.props.name}</h1>
-                <ul>
-                    <li style={greenStyle}>Learn React</li>
-                    <Undone>Pass Programming Laboratoty</Undone>
-                    <Todo value ="Have a nice holiday"
-                          done = {false}   />
-                </ul>
+                {todos}
+                <p> My new todo </p>
+                <input
+                    type='text'
+                    onChange={this.myChangeHandler}
+                />
+                <button onClick={this.addTodo}>
+                    AddTodo
+                </button>
             </div>
         );
     }
